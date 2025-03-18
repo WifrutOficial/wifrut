d2a0ec3d50d650f8d82acb5acb69e0967d853e91
mai1209 <mairacoria1209@gmail.com>
2025-03-18 00:55:27 -0300

    ultimo arreglo vercel activo

--- frontend/wifrut/vite.config.js
+++ frontend/wifrut/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Aquí defines que Vite generará la carpeta "dist"
+    outDir: 'dist', // Directorio de salida para la build
+    rollupOptions: {
+      input: './index.html' // El punto de entrada de la aplicación
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000', // Si estás desarrollando el backend localmente
+    }
   }
 });
