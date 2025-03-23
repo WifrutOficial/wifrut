68136798b922e9742ffde690ad81ca3e42cf7940
mai1209 <mairacoria1209@gmail.com>
2025-03-22 22:36:41 -0300

    arreglo cors

--- backend/server.js
+++ backend/server.js
@@ -2,66 +2,55 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import  adminRoutes  from "./routes/adminRoutes.js";
-import mayoristaRoutes from "./routes/mayoristaRoutes.js"
-import minoristaRoutes from "./routes/minoristaRoutes.js"
-import productsRoutes from "./routes/productsRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import  whatsAppRoutes from "./routes/whatsAppRoutes.js"
-import cookieParser from "cookie-parser"
+import adminRoutes from "./routes/adminRoutes.js";
+import mayoristaRoutes from "./routes/mayoristaRoutes.js";
+import minoristaRoutes from "./routes/minoristaRoutes.js";
+import productsRoutes from "./routes/productsRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import whatsAppRoutes from "./routes/whatsAppRoutes.js";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./database/db.js";
-import mercadoPagoRoutes from "./routes/mercadoPagoRoutes.js"
-
-
+import mercadoPagoRoutes from "./routes/mercadoPagoRoutes.js";
 
 // Configuración de dotenv para acceder a las variables de entorno
 dotenv.config();
 
 const app = express();
 
-//conexion con el forntend
+// Configuración de CORS
 app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? process.env.VITE_FRONTEND_URL_PROD
-        : process.env.FRONTEND_URL_DEV,
+        ? process.env.VITE_FRONTEND_URL_PROD  // URL de tu frontend en producción
+        : process.env.FRONTEND_URL_DEV,      // URL de tu frontend en desarrollo
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    credentials: true, // Necesario si se usan cookies o encabezados de autorización
   })
 );
 
-//middleware
+// Middleware para el manejo de JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// ejecucuion de la conexion a la base de datos
+
+// Conexión a la base de datos
 connectDB();
 
-//manejo de cookies
-app.use(cookieParser())
+// Manejo de cookies
+app.use(cookieParser());
 
-//rutas
+// Rutas del backend
 app.use("/api", authRoutes);
-app.use("/api/admin", adminRoutes)
-app.use("/api/mayorista", mayoristaRoutes)
-app.use("/api/minorista",minoristaRoutes)
-app.use("/api/products", productsRoutes)
-app.use("/api/order" , orderRoutes)
-app.use("/api/whatsapp", whatsAppRoutes )
-app.use("/api/mercadopago",mercadoPagoRoutes)
+app.use("/api/admin", adminRoutes);
+app.use("/api/mayorista", mayoristaRoutes);
+app.use("/api/minorista", minoristaRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/whatsapp", whatsAppRoutes);
+app.use("/api/mercadopago", mercadoPagoRoutes);
 
-
-//servidor
-const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== "production") {
-  // Solo en desarrollo local, inicia el servidor.
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-
+// Ruta raíz
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Exporta la aplicación para Vercel en producción.
+// Exporta la aplicación para ser utilizada en Vercel
 export default app;
