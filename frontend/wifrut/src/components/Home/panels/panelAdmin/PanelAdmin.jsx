3ba1fbfc570b2137f0374679a61d11a3ddb7e76f
mai1209 <mairacoria1209@gmail.com>
2025-04-01 23:41:25 -0300

    admin

--- frontend/wifrut/src/components/Home/panels/panelAdmin/PanelAdmin.jsx
+++ frontend/wifrut/src/components/Home/panels/panelAdmin/PanelAdmin.jsx
@@ -5,43 +5,65 @@ import { useState } from "react";
 import Products from "./Products";
 import PanelMayorista from "./PanelMayorista";
 import MayoristasAprobados from "./MayoristasAprobados";
-import { PiArrowBendDownLeftThin } from "react-icons/pi";
+import { GoArrowUp } from "react-icons/go";
 import BuscarPedidos from "./BuscarPedidos";
 
 function PanelAdmin() {
   const navigate = useNavigate();
 
-  const [activeComponent, setActiveComponent] = useState(null)
+  const [activeComponent, setActiveComponent] = useState(null);
 
   return (
     <div className={style.container}>
-      <div className={style.nav}>
-        <div className={style.navContent}>
-        <img className={style.logo} src="../../../../../logo.png" alt="logo" />
-        <p className={style.title}>Panel administrador</p>
-        <button onClick={() =>setActiveComponent(<Products></Products>)}>
-          Carga de Productos
-        </button>
-        <button onClick={() => setActiveComponent(<PanelMayorista></PanelMayorista>)}>
-          Solicitudes Pendientes
-        </button>
-        <button onClick={() => setActiveComponent(<MayoristasAprobados></MayoristasAprobados>)}>
-          Lista Clientes Mayoristas
-        </button>
-        <button onClick={() => setActiveComponent(<BuscarPedidos></BuscarPedidos>)}>
-          Buscar pedidos
-        </button>
-        </div>
-      
-      </div>
+      <p className={style.title}>Panel administrador</p>
+      <div className={style.panelContainer}>
+        <div className={style.nav}>
+          <div className={style.navContent}>
+            <img
+              className={style.logo}
+              src="../../../../../logo.png"
+              alt="logo"
+            />
 
-      <div>
-      {activeComponent || (
-        <div className={style.inicialContainer}>
-          <p>Selecciona una opción del panel </p>
-          <PiArrowBendDownLeftThin />
+            <div className={style.buttons}>
+              <button onClick={() => setActiveComponent(<Products></Products>)}>
+                Carga de Productos
+              </button>
+              <button
+                onClick={() =>
+                  setActiveComponent(<PanelMayorista></PanelMayorista>)
+                }
+              >
+                Solicitudes Pendientes
+              </button>
+              <button
+                onClick={() =>
+                  setActiveComponent(
+                    <MayoristasAprobados></MayoristasAprobados>
+                  )
+                }
+              >
+                Lista Clientes Mayoristas
+              </button>
+              <button
+                onClick={() =>
+                  setActiveComponent(<BuscarPedidos></BuscarPedidos>)
+                }
+              >
+                Buscar pedidos
+              </button>
+            </div>
+          </div>
         </div>
-      )}
+
+        <div>
+          {activeComponent || (
+            <div className={style.inicialContainer}>
+              <p>Selecciona una opción del panel </p>
+              <GoArrowUp />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
