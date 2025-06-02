afaa4c02e48d6dfd758b3937caa1e0d66f0e6ada
mai1209 <mairacoria1209@gmail.com>
2025-06-02 14:22:50 -0300

    cokkie en header y samesite elimiando

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
   },
   credentials: true,
    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-  allowedHeaders: ["Content-Type", "Authorization"], 
+  allowedHeaders: ["Content-Type", "Authorization","Cookie"], 
 };
 
 app.use(cors(corsOptions));
